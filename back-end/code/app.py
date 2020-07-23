0182b516d1a60184c7caa856072cd7d294ac7f37
HanselD <hansel96@gmail.com>
2020-07-22 20:07:23 -0400

    working lambda, needs env var for table name

--- back-end/code/app.py
+++ back-end/code/app.py
@@ -1,11 +1,31 @@
 import boto3
 import json
 
+# initialize boto3 resources
+dynamo = boto3.resource('dynamodb')
+table = dynamo.Table('test')
 
 def lambda_handler(event, context):
     #get current value from ddb
+    resp = table.get_item(Key={'id':1})
+    current = resp['Item']['visitors']
+
     #add 1
-    count=0
+    current += 1
+    
     #update ddb
+    table.update_item(
+       Key={'id':1},
+       UpdateExpression='SET visitors = :val1',
+       ExpressionAttributeValues={
+            ':val1': current
+        }
+    )
+
     #return to website
-    return count
+    return {
+        'count': current,
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
