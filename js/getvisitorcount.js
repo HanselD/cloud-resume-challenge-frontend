6999ece690c1f6bda621a80bc5aca54abaeb08f9
HanselD <hansel96@gmail.com>
2020-07-24 00:56:18 -0400

    testing ajax again

--- js/getvisitorcount.js
+++ js/getvisitorcount.js
@@ -1,7 +1,8 @@
 $.ajax({
     url: "https://srbogtrqw6.execute-api.us-east-1.amazonaws.com/Prod/count",
-    type: 'POST',
-    contentType: 'application/json'    
-  }).done(function(data) {
-    $('#monitor_data').append(data.body.message)
+    method: 'POST',
+    contentType: 'application/json',
+    success: function(response) {
+        $('#count').append(response.message)
+    }   
   });
